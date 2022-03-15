d0dd8cb509e1ac6b6c14aab39599eb8662267cc2
Leo113 <leo2.scomazon@gmail.com>
2022-03-15 11:01:48 +0100

    Comments

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -1,5 +1,8 @@
 package ma.enset;
 
+/**
+ * On ajoute des commentaires
+ */
 public class Test {
     public static void main(String[] args) {
         System.out.println("Ceci est un message ...");
